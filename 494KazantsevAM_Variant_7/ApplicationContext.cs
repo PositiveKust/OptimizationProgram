b8ac7aba4f8abd9938846c95d7be6de2bd4bcd62
PositiveKust <38491755+PositiveKust@users.noreply.github.com>
2023-04-24 23:51:09 +0300

    Create ApplicationContext.cs

---
+++ 494KazantsevAM_Variant_7/ApplicationContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace _494KazantsevAM_Variant_7
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Model> Models { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<OptimizationMethod> OptimizationMethods { get; set; }
+        public ApplicationContext() : base("DefaultConnection") { }
+    }
+}
