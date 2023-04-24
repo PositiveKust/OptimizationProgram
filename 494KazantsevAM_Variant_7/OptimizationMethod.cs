379c4cf39a1337fc04a6d440e74bd872fb0f7d26
PositiveKust <38491755+PositiveKust@users.noreply.github.com>
2023-04-24 23:50:32 +0300

    Create OptimizationMethod.cs

---
+++ 494KazantsevAM_Variant_7/OptimizationMethod.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _494KazantsevAM_Variant_7
+{
+    public class OptimizationMethod
+    {
+        public int id { get; set; }
+        private string name;
+        private string active;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Active
+        {
+            get { return active; }
+            set { active = value; }
+        }
+        public OptimizationMethod() { }
+        public OptimizationMethod(string name, string active)
+        {
+            this.name = name;
+            this.active = active;
+        }
+    }
+}
