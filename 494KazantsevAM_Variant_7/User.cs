e3b7f22c52b6de164071978c3af6bf835eb5e58a
PositiveKust <38491755+PositiveKust@users.noreply.github.com>
2023-04-24 23:50:53 +0300

    Create User.cs

---
+++ 494KazantsevAM_Variant_7/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _494KazantsevAM_Variant_7
+{
+    public class User
+    {
+        public int id { get; set; }
+        private string name, password;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+        public User() { }
+        public User(string name, string password)
+        {
+            this.name = name;
+            this.password = password;
+        }
+    }
+}
